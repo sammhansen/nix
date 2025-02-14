59be085ef9e0bab5160148fb41cf6cdd4964b703
Hansen <sammhansen@gmail.com>
2025-02-14 20:57:40 +0300

    feat: add wezterm cachix

--- modules/orion/nix.nix
+++ modules/orion/nix.nix
@@ -26,12 +26,14 @@
       # fetch from a cache if we can
       substituters = [
         "https://hyprland.cachix.org"
+        "https://wezterm.cachix.org"
         "https://nix-community.cachix.org"
         "https://cosmic.cachix.org"
       ];
 
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       ];
